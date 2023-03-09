e575fd11195fbc130525eb16d1882b4179c84be2
alexcapricorngmailcom <107393171+alexcapricorngmailcom@users.noreply.github.com>
2023-03-09 16:24:36 +0200

    add task1

---
+++ main.js
@@ -0,0 +1,20 @@
+const list = document.querySelectorAll('li');
+
+list.forEach(el => {
+    el.addEventListener('click', function(event) {
+        const active = document.querySelector('.active');
+        
+        if (active) {
+            active.classList.remove('active');
+        }
+        
+        const target = event.currentTarget;
+        target.classList.add('active');
+
+        const ctrl = event.ctrlKey;
+
+        if (ctrl) {
+            active.classList.add('active');
+        }
+    });
+})
